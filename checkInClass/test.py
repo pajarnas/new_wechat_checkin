16ac0da948ad86d3049cdb56d5e1731ab603928a
pajamas <444623630@qq.com>
2017-06-06 18:04:03 +0800

    True third

--- checkInClass/test.py
+++ checkInClass/test.py
@@ -11,7 +11,7 @@ class TimerDemo():
          self.t.start()
 
 if __name__ == '__main__':
-    print 'hello first col 23'
+    print 'after push failed,and pull failed, then i checkout first version ,then i updated this line try to push!AGAIN!'
     while(True):
         t.run()
 
