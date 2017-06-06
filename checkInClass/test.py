91ff819ab5c31f4ab39969f21373ed629f89a763
pajamas <444623630@qq.com>
2017-06-06 18:21:24 +0800

    modifed fourth

--- checkInClass/test.py
+++ checkInClass/test.py
@@ -11,7 +11,7 @@ class TimerDemo():
          self.t.start()
 
 if __name__ == '__main__':
-    print 'after push failed,and pull failed, then i checkout first version ,then i updated this line try to push!AGAIN!'
+    print 'fourth!'
     while(True):
         t.run()
 
